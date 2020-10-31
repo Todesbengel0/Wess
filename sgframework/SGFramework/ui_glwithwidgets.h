d3233b429a3ccc9e730b984a80da0b9ad6db7e99
Vinz <funk.vin@t-online.de>
2020-10-31 17:06:36 +0100

    Visual Studio setup

--- sgframework/SGFramework/ui_glwithwidgets.h
+++ sgframework/SGFramework/ui_glwithwidgets.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'glwithwidgets.ui'
 **
-** Created by: Qt User Interface Compiler version 5.6.0
+** Created by: Qt User Interface Compiler version 5.14.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,11 +10,8 @@
 #define UI_GLWITHWIDGETS_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QWidget>
 #include "openglwidget.h"
@@ -31,7 +28,7 @@ public:
     void setupUi(QMainWindow *GLwithWidgets)
     {
         if (GLwithWidgets->objectName().isEmpty())
-            GLwithWidgets->setObjectName(QStringLiteral("GLwithWidgets"));
+            GLwithWidgets->setObjectName(QString::fromUtf8("GLwithWidgets"));
         GLwithWidgets->resize(1000, 600);
         QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         sizePolicy.setHorizontalStretch(0);
@@ -40,16 +37,17 @@ public:
         GLwithWidgets->setSizePolicy(sizePolicy);
         GLwithWidgets->setMinimumSize(QSize(600, 400));
         RootWidget = new QWidget(GLwithWidgets);
-        RootWidget->setObjectName(QStringLiteral("RootWidget"));
+        RootWidget->setObjectName(QString::fromUtf8("RootWidget"));
         sizePolicy.setHeightForWidth(RootWidget->sizePolicy().hasHeightForWidth());
         RootWidget->setSizePolicy(sizePolicy);
         RootWidget->setMaximumSize(QSize(16777215, 16777215));
         horizontalLayout = new QHBoxLayout(RootWidget);
         horizontalLayout->setSpacing(6);
         horizontalLayout->setContentsMargins(11, 11, 11, 11);
-        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
         myGLWidget = new OpenGLWidget(RootWidget);
-        myGLWidget->setObjectName(QStringLiteral("myGLWidget"));
+        myGLWidget->setObjectName(QString::fromUtf8("myGLWidget"));
         QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Expanding);
         sizePolicy1.setHorizontalStretch(20);
         sizePolicy1.setVerticalStretch(20);
@@ -59,6 +57,7 @@ public:
         myGLWidget->setMaximumSize(QSize(10000, 10000));
         myGLWidget->setSizeIncrement(QSize(1, 1));
         myGLWidget->setBaseSize(QSize(600, 400));
+        myGLWidget->setMouseTracking(true);
         myGLWidget->setFocusPolicy(Qt::StrongFocus);
         myGLWidget->setLayoutDirection(Qt::LeftToRight);
 
@@ -73,7 +72,7 @@ public:
 
     void retranslateUi(QMainWindow *GLwithWidgets)
     {
-        GLwithWidgets->setWindowTitle(QApplication::translate("GLwithWidgets", "QtGL", 0));
+        GLwithWidgets->setWindowTitle(QCoreApplication::translate("GLwithWidgets", "QtGL", nullptr));
     } // retranslateUi
 
 };
