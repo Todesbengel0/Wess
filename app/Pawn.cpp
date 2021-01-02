2b4d677eeb5e0828abc7893ec16708176a6b0c56
Todesbengel <dan@eisenkraemer.com>
2021-01-02 17:16:37 +0100

    There is no turning back for Pawns

--- app/Pawn.cpp
+++ app/Pawn.cpp
@@ -7,8 +7,8 @@ Pawn::~Pawn() = default;
 
 bool Pawn::ValidMovement(int xStartPosition, int zStartPosition, int xTargetPosition, int zTargetPosition)
 {
-    if ((abs(zTargetPosition-zStartPosition)==1 && abs(xTargetPosition-xStartPosition)<=1)
-            || ((zStartPosition == 1 || zStartPosition == 6) && abs(zStartPosition-zTargetPosition)==2 && xStartPosition-xTargetPosition==0))
+    if ((zStartPosition-zTargetPosition==((this->GetFieldColor()==White)?-1:1) && abs(xTargetPosition-xStartPosition)<=1)
+            || ((zStartPosition == 1 || zStartPosition == 6) && zStartPosition-zTargetPosition==((this->GetFieldColor()==White)?-2:2) && xStartPosition-xTargetPosition==0))
         return true;
     return false;
 }
